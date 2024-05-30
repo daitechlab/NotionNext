cbef575efd484c6cddc008756ed8d125cfa2b6cd
Admin <dth1995911@163.com>
2024-05-30 22:29:32 +0800

    update

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -4,15 +4,22 @@ import { AnalyticsCard } from './AnalyticsCard'
 import Card from './Card'
 import Catalog from './Catalog'
 import { InfoCard } from './InfoCard'
-<<<<<<< HEAD
-import dynamic from 'next/dynamic'
-import { AnalyticsCard } from './AnalyticsCard'
-import TouchMeCard from './TouchMeCard'
-=======
->>>>>>> sync
 import LatestPostsGroupMini from './LatestPostsGroupMini'
 import TagGroups from './TagGroups'
 import TouchMeCard from './TouchMeCard'
+
+const FaceBookPage = dynamic(
+  () => {
+    let facebook = <></>
+    try {
+      facebook = import('@/components/FacebookPage')
+    } catch (err) {
+      console.error(err)
+    }
+    return facebook
+  },
+  { ssr: false }
+)
 
 /**
  * Hexo主题右侧栏
@@ -37,34 +44,8 @@ export default function SideRight(props) {
           </Card>
         )}
 
-<<<<<<< HEAD
-            <div className='sticky top-20 space-y-4'>
-
-                {/* 文章页显示目录 */}
-                {post && post.toc && post.toc.length > 0 && (
-                    <Card className='bg-white dark:bg-[#1e1e1e]'>
-                        <Catalog toc={post.toc} />
-                    </Card>
-                )}
-
-                {/* 最新文章列表 */}
-                <div className={'border dark:border-gray-700 dark:bg-[#1e1e1e] dark:text-white rounded-xl lg:p-6 p-4 hidden lg:block bg-white'}>
-                    <LatestPostsGroupMini {...props} />
-                </div>
-
-                {rightAreaSlot}
-
-                {/* 标签和成绩 */}
-                <Card className={'bg-white dark:bg-[#1e1e1e] dark:text-white'}>
-                    <TagGroups tags={tagOptions} currentTag={currentTag} />
-                    <hr className='mx-1 flex border-dashed relative my-4' />
-                    <AnalyticsCard {...props} />
-                </Card>
-            </div>
-=======
         {/* 联系交流群 */}
         <TouchMeCard />
->>>>>>> sync
 
         {/* 最新文章列表 */}
         <div
